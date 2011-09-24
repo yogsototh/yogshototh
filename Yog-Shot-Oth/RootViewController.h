5601e1a32dd56eff5ebbdf0a4c837e845a6d1da5
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2011-09-24 09:15:59 +0200

    Initial Commit

---
+++ Yog-Shot-Oth/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Yog-Shot-Oth
+//
+//  Created by Yann Esposito on 24/09/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ Yog-Shot-Oth/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Yog-Shot-Oth
+//
+//  Created by Yann Esposito on 24/09/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
